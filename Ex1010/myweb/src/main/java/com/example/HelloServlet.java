537d0a66a403374b88be7cc4c15bb1b4796170b4
최수빈 <165890322+soob0513@users.noreply.github.com>
2024-10-10 12:37:32 +0900

    1010

---
+++ Ex1010/myweb/src/main/java/com/example/HelloServlet.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = resp.getWriter();
+        out.println("<H2>Hello Servlet!<H2>");
+        out.close();
+    }
+}
